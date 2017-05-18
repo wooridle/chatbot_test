08f639abbb815410e347b83b62b2b9d9485c1975
wooridle <urleee@naver.com>
2017-05-19 00:48:19 +0900

    app.py modified

--- app.py
+++ app.py
@@ -25,8 +25,12 @@ def chat_response(query):
     query_noun = kkma.nouns(query)
     response = None
 
+    print(query_noun)
     if "안녕" in query_noun:
         response = '안녕하세요!'
+
+    if "교열" and ("가격" or "견적") in query_noun:
+        response = "3장 기준으로 5천원입니다.\n3장 이상의 경우에는 8천원입니다."
 
     return json.jsonify(response = response)
 
